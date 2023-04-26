1ff3a111d76c294575e5dbd54ef3262e8077694a
dinesh-geek <110802323+INC02113@users.noreply.github.com>
2023-04-26 14:12:30 +0530

    "added "

--- shimano/src/main/java/com/incture/shimano/Config/CloudConfig.java
+++ shimano/src/main/java/com/incture/shimano/Config/CloudConfig.java
@@ -1,70 +1,89 @@
-//package com.incture.shimano.Config;
+package com.incture.shimano.Config;
+
+
+import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
+import com.sap.cloud.security.xsuaa.token.TokenAuthenticationConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+@Configuration
+@EnableWebSecurity
+public class CloudConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    XsuaaServiceConfiguration xsuaaServiceConfiguration;
+
+
+//	@Autowired
+//	XsuaaTokenFlows xsuaaTokenFlows;
+
+//    @Bean
+//    public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(final ReactiveClientRegistrationRepository clientRegistrationRepository,
+//                                                                         final ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+//        ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider = ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+//                .clientCredentials()
+//                .build();
 //
-//import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
-//import com.sap.cloud.security.xsuaa.token.TokenAuthenticationConverter;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.convert.converter.Converter;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AbstractAuthenticationToken;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.oauth2.jwt.Jwt;
+//        DefaultReactiveOAuth2AuthorizedClientManager authorizedClientManager = new DefaultReactiveOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository);
 //
-//@Configuration
-//@EnableWebSecurity
-//public class CloudConfig extends WebSecurityConfigurerAdapter{
-//
-//    @Autowired
-//    XsuaaServiceConfiguration xsuaaServiceConfiguration;
-//
-//
-////	@Autowired
-////	XsuaaTokenFlows xsuaaTokenFlows;
-//
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        // @formatter:off
-//        try {
-//            System.err.println("SecurityFilterChain try: ");
-//            http
-//                    .csrf().disable()
-//                    .sessionManagement()
-//                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session is created by approuter
-//                    .and()
-//                    .authorizeRequests()
-////			.antMatchers("/swagger-ui.html#/**").permitAll()
+//        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+//        return authorizedClientManager;
+//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        try {
+            System.err.println("SecurityFilterChain try: ");
+            http
+                    .csrf().disable()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session is created by approuter
+                    .and()
+                    .authorizeRequests()
+			        .antMatchers("/swagger-ui.html#/**").permitAll()
 //                    .antMatchers("/**").permitAll()
-//                    .anyRequest().authenticated()
-//                    .and()
-//                    .oauth2ResourceServer()
-//                    .jwt()
-//                    .jwtAuthenticationConverter(getJwtAuthenticationConverter());
-//        }catch(Exception e) {
-//            System.err.println("SecurityFilterChain: "+e.getMessage());
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    /**
-//     * Customizes how GrantedAuthority are derived from a Jwt
-//     */
-//    Converter<Jwt, AbstractAuthenticationToken> getJwtAuthenticationConverter() {
-//        TokenAuthenticationConverter converter = new TokenAuthenticationConverter(xsuaaServiceConfiguration);
-//        converter.setLocalScopeAsAuthorities(true);
-//        return converter;
-//    }
-//
-//    @Override
-//    public void configure(WebSecurity web) throws Exception
-//    {
-//        web.ignoring().mvcMatchers(HttpMethod.OPTIONS, "/**");
-//        web.ignoring().mvcMatchers("/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**",
-//                "/v2/api-docs", "/webjars/**");
-//    }
-//
-//}
+                    .anyRequest().authenticated()
+                    .and()
+                    .oauth2ResourceServer()
+                    .jwt()
+                    .jwtAuthenticationConverter(getJwtAuthenticationConverter());
+        }catch(Exception e) {
+            System.err.println("SecurityFilterChain: "+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Customizes how GrantedAuthority are derived from a Jwt
+     */
+    Converter<Jwt, AbstractAuthenticationToken> getJwtAuthenticationConverter() {
+        TokenAuthenticationConverter converter = new TokenAuthenticationConverter(xsuaaServiceConfiguration);
+        converter.setLocalScopeAsAuthorities(true);
+        return converter;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception
+    {
+        web.ignoring().mvcMatchers(HttpMethod.OPTIONS, "/**");
+        web.ignoring().mvcMatchers("/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**",
+                "/v2/api-docs", "/webjars/**");
+    }
+
+}
--- shimano/src/main/java/com/incture/shimano/Config/WebSecurityConfig.java
+++ shimano/src/main/java/com/incture/shimano/Config/WebSecurityConfig.java
@@ -1,33 +1,33 @@
-package com.incture.shimano.Config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-
-@SuppressWarnings("deprecation")
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().
-                and().
-                authorizeRequests().
-                antMatchers(HttpMethod.GET, "/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PATCH, "/*").hasRole("ADMIN")
-                .and().csrf().disable();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}cpi@1234").roles("ADMIN");
-    }
-}
-
+//package com.incture.shimano.Config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//
+//@SuppressWarnings("deprecation")
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic().
+//                and().
+//                authorizeRequests().
+//                antMatchers(HttpMethod.GET, "/*").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.POST, "/*").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.PUT, "/*").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.PATCH, "/*").hasRole("ADMIN")
+//                .and().csrf().disable();
+//    }
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("user").password("{noop}cpi@1234").roles("ADMIN");
+//    }
+//}
+//
